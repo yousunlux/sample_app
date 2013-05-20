460414bc9d03824057ea8304cd5ae9d799027a3e
You Sun Lux EA <you.ea@thalesgroup.com>
2013-05-20 13:54:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ea6e4e51b02af4ee66048180db82b50efe2396ca991d2848db686dbff553a3103854586be39eec8c5b064873c15b35e5134d717548e443b40f5b81827449ed93'
